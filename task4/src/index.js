2769f35c5da9dd42812ef43d0ec4e21c3d5e7f38
nsigi <nik666564@gmail.com>
2024-01-06 19:52:04 +0400

    removed ajax, used native js, localStorage

--- task4/src/index.js
+++ task4/src/index.js
@@ -1,93 +1,65 @@
-(function onLoad(){
-  const itemsContainer = document.querySelector(".cards");
-  getItems().then((items) => items.map((res) => itemsContainer.append(addCard(res)))); 
+(function onLoad() {
+  const cards = window.document.querySelector(".cards");
+  let items = JSON.parse(window.localStorage.getItem("items"));
+  items.map((item) => cards.append(addCard(item)));
 }());
 
 const btnSetup = document.getElementById('btn-setup');
 btnSetup.addEventListener('click', () => {
-   const data = [
-    {
-      "id": 1,
-      "name": "Огурец",
-      "description": "Свежий, зеленый",
-      "img": "imgs/cucumber.jpg",
-      "code": "0237",
-      "provider": "Волжская грядка"
-    },
-    {
-      "id": 2,
-      "name": "Помидор",
-      "description": "Красный, идеальный",
-      "img": "imgs/tomat.jpg",
-      "code": "3422",
-      "provider": "Африканская грядка"
-    },
-    {
-      "id": 3,
-      "name": "Капуста",
-      "description": "Один початок",
-      "img": "imgs/cabbage.png",
-      "code": "3453",
-      "provider": "Поля Франции"
-    }
-   ];
-
-   deleteAll().then(() => data.forEach(item => createItem(item))).then(() => window.location.reload())
+    const data = [
+      {
+        "name": "Огурец",
+        "description": "Свежий, зеленый",
+        "img": "imgs/cucumber.jpg",
+        "code": "0237",
+        "provider": "Волжская грядка"
+      },
+      {
+        "name": "Помидор",
+        "description": "Красный, идеальный",
+        "img": "imgs/tomat.jpg",
+        "code": "3422",
+        "provider": "Африканская грядка"
+      },
+      {
+        "name": "Капуста",
+        "description": "Один початок",
+        "img": "imgs/cabbage.png",
+        "code": "3453",
+        "provider": "Поля Франции"
+      }
+    ];
+    const cards = window.document.querySelector(".cards");
+    data.map((item) => cards.append(addCard(item)));
+    window.localStorage.setItem("items", JSON.stringify(data));
+    window.location.reload();
   });
 
-let isEdit = false;
 const form = document.querySelector('.main-form');   
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (!isEdit) {
-      const obj = {};
-      const inputs = e.target.querySelectorAll("input");
-      inputs.forEach((item) => (obj[item.id] = item.value));
-      createItem(obj);
-      inputs.forEach((item) => (item.value = ""));
-    }
-  });
+form.addEventListener("submit", addItem);
+function addItem(e){
+  e.preventDefault();
+  const cards = window.document.querySelector(".cards");
+  const items = JSON.parse(window.localStorage.getItem("items"));
+  const item = {};
+  const inputs = e.target.querySelectorAll("input");
+  inputs.forEach((res) => (item[res.id] = res.value));
+  items.push(item);
+  window.localStorage.setItem("items", JSON.stringify(items));
+  //console.log("addItem", item);
+  cards.append(addCard(item));
+  inputs.forEach((res) => (res.value = ""));
+};
 
-async function getItems(){
-  const items = await fetch('http://localhost:3000/items');
-  const data = await items.json();
-  return data;
-}
-
-async function getItem(id){
-  const item = await fetch(`http://localhost:3000/items/${id}`);
-  const data =  item.json();
-  return data;
-}
-
-function createItem(obj){
-  const cardsContainer = document.querySelector(".cards");
-  addItem(obj).then((item) => cardsContainer.append(addCard(item)));
-}
-
-async function addItem(data){
-  const item = await fetch('http://localhost:3000/items', 
-  {
-    method:'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
-  },
-  body:JSON.stringify(data)}).then((item) => 
-  {
-    return item.json()
-  }).catch((e) => console.error(e));
-  return item;
-}
-
-function addCard({name, img, description, id, provider, code}) {
+function addCard(item) {
   const cardImg = document.createElement('img');
   cardImg.classList.add('card-img');
-  cardImg.src = img;
+  cardImg.src = item.img;
   cardImg.style.width = "100px";
   
   const cardName = document.createElement("h5");
   cardName.classList.add("card-name");
-  cardName.append(document.createTextNode(name));
+  cardName.append(document.createTextNode(item.name));
   cardName.style.display = "flex";
   
   let boldText = document.createElement('strong');
@@ -95,7 +67,7 @@ function addCard({name, img, description, id, provider, code}) {
   cardDescription.classList.add("card-description");
   boldText.append(document.createTextNode("Описание: "));
   cardDescription.append(boldText);
-  cardDescription.append(document.createTextNode(description));
+  cardDescription.append(document.createTextNode(item.description));
   cardDescription.style.position = "relative";
   
   boldText = document.createElement('strong');
@@ -103,26 +75,19 @@ function addCard({name, img, description, id, provider, code}) {
   cardProvider.classList.add("card-provider");
   boldText.append(document.createTextNode("Поставщик: "));
   cardProvider.append(boldText);
-  cardProvider.append(document.createTextNode(provider));
+  cardProvider.append(document.createTextNode(item.provider));
   
   boldText = document.createElement('strong')
   const cardCode = document.createElement("p");
   cardCode.classList.add("card-code");
   boldText.append(document.createTextNode("Код товара: "));
   cardCode.append(boldText);    
-  cardCode.append(document.createTextNode(code));
-
-  boldText = document.createElement('strong');
-  const cardId = document.createElement("p");
-  cardId.classList.add("id");
-  boldText.append(document.createTextNode("ID: "));
-  cardId.append(boldText);
-  cardId.append(document.createTextNode(id));
+  cardCode.append(document.createTextNode(item.code));
 
   const buttons = document.createElement("div");
   buttons.classList.add("btns-change");
-  const editBtn = createButton(true, name, img, description, id, provider);
-  const deleteBtn = createButton(false, name, img, description, id, provider);
+  const editBtn = createButton(true, item);
+  const deleteBtn = createButton(false, item);
   buttons.append(editBtn);
   buttons.append(deleteBtn);
   
@@ -141,8 +106,7 @@ function addCard({name, img, description, id, provider, code}) {
   const card = document.createElement("div");
   card.style.width = '18rem';
   card.classList.add("card");
-  card.id = id;
-  card.append(cardId);
+  card.id = item.code;
   card.append(info);
   card.append(infoAdd);
   card.append(buttons);
@@ -150,12 +114,12 @@ function addCard({name, img, description, id, provider, code}) {
   return card;
 }
 
-const createButton = (isEdit, name, img, description, id) => {
-  let textNode = "Delete";
+function createButton(isEdit, item){
+  let textNode = "Удалить";
   let classBtn = "btn-danger";
   if(isEdit)
   {
-    textNode = "Edit";
+    textNode = "Изменить";
     classBtn = "btn-primary";
   }
   const newBtn = document.createElement("button");
@@ -164,11 +128,11 @@ const createButton = (isEdit, name, img, description, id) => {
     e.preventDefault();
     if(isEdit)
     {
-      getItem(id).then((res) => editItem(res, id));
+      editItem(item);
     }
     else
     {
-      deleteItem(id);
+      deleteItem(item.code);
     }
   });
   newBtn.append(document.createTextNode(textNode));
@@ -177,71 +141,35 @@ const createButton = (isEdit, name, img, description, id) => {
   return newBtn;
 }
 
-function editItem(data, id) {
-  isEdit = true;
+let curEditCode;
+function editItem(data) {
   const inputs = form.querySelectorAll("input");
   inputs.forEach((item) => (item.value = data[item.id]));
   const saveBtn = document.getElementById("btn-save");
   saveBtn.textContent = "Редактировать";
-  form.addEventListener('submit', (e) =>
-  {
+  form.removeEventListener("submit", addItem);
+  form.addEventListener('submit', editCard);
+  curEditCode = data.code;
+}
+
+function editCard(e){
     e.preventDefault();
-    const obj = {}; 
-    inputs.forEach((item) => (obj[item.id] = item.value));
-    obj.id = id;  
-    editCard(obj);
-    inputs.forEach((item) => (item.value = ""));
-    const itemToChange = document.getElementById(data.id);
-    itemToChange.querySelector('.card-img').src = obj.img;
-    itemToChange.querySelector('.card-name').textContent = obj.name;
-
-    let boldText = document.createElement('strong');
-    boldText.textContent = "Описание: ";
-    itemToChange.querySelector('.card-description').textContent = "";
-    itemToChange.querySelector('.card-description').append(boldText, obj.description);
-
-    boldText = document.createElement('strong');
-    boldText.textContent = "Поставщик: ";
-    itemToChange.querySelector('.card-provider').textContent = "";
-    itemToChange.querySelector('.card-provider').append(boldText, obj.provider);
-
-    boldText = document.createElement('strong');
-    boldText.textContent = "Код товара: ";
-    itemToChange.querySelector('.card-code').textContent = "";
-    itemToChange.querySelector('.card-code').append(boldText, obj.code);
+    deleteItem(curEditCode);
+    addItem(e);
     
+    form.removeEventListener("submit", editCard);
+    const saveBtn = document.getElementById("btn-save");
     saveBtn.textContent = "Добавить";
-    isEdit = false;
-  }, {once:true});
+    form.addEventListener("submit", addItem);  
+  }
+
+function deleteItem(code) {
+  let card = document.getElementById(code);
+  let items = JSON.parse(window.localStorage.getItem("items"));
+  //console.log(cards);
+  const otherItems = items.filter((item) => item.code !== code);
+  //console.log("Other", otherItems);
+  window.localStorage.setItem("items", JSON.stringify(otherItems));
+  card.remove();
 }
-
-async function editCard(data){
-  await fetch(`http://localhost:3000/items/${data.id}`, 
-  {
-    method:'PATCH',
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
-  },
-  body: JSON.stringify(data)
-  }).then((item) =>  
-  {
-    return item.json()
-  }).catch((e) => console.error(e));
-}
-
-  function deleteItem(id) {
-    const item = document.getElementById(id);
-    deleteItemM(id);
-    item.remove();
-  }
-
-  async function deleteItemM(id){
-    await fetch(`http://localhost:3000/items/${id}`,{
-      method: 'DELETE'
-    });
-  }
-
-  function deleteAll(){
-    return getItems().then((data) => data.forEach((item) => deleteItemM(item.id)))
-  }
   
